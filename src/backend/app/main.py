1697ed17c146997605b103261d2edf20f0aeabd3
Khthonian <anansicorp@tuta.io>
2024-02-18 23:39:58 +0000

    feat(src): Implement frontend
    A frontend has no been implemented using React. It's a simple solution
    that displays the message from the root route. This will be
    significantly built upon, to be made into the application.

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,24 +1,28 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+origins = ["http://localhost:3000", "localhost:3000"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Define the root route
-@app.get("/")
+@app.get("/", tags=["root"])
 async def root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, Charlie!"}
 
 
 # Define the test route
 @app.get("/test/{name}")
 async def test(name):
     return {f"Hello, {name}!"}
-
-
-def main():
-    uvicorn.run("main:app", port=5000)
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching message:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Message from FastAPI Server:</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
